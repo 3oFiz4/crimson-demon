7a86169ab07efc6d244a141671dc988204b104d4
3ofiz4 <boolalgorithm@gmail.com>
2026-05-21 19:56:20 +0800

    Added new color variables to align with .groups

--- lua/crimson-demon/palette.lua
+++ lua/crimson-demon/palette.lua
@@ -1,76 +1,113 @@
 return {
-  -- custom_temporary
+  -- Base colors
+  black = "#000000",
   red = "#ff0000",
   orange = "#ff6200",
   white = "#f2f2f2",
+
+  -- =========================================================
+  -- Brightness modifiers
+  -- You can tweak ONLY these if you want different contrast.
+  -- =========================================================
+
   bg_0 = "#000000",
   bg_1 = "#080808",
   bg_2 = "#121212",
   bg_3 = "#1a1a1a",
   bg_4 = "#262626",
+
   fg_0 = "#666666",
   fg_1 = "#8a8a8a",
   fg_2 = "#bcbcbc",
   fg_3 = "#d0d0d0",
   fg_4 = "#eeeeee",
+
   red_dark = "#870000",
   red_mid = "#af0000",
   red_soft = "#d70000",
+
   orange_soft = "#ff6200",
   orange_dim = "#cc4e00",
+
+  -- =========================================================
+  -- Element approach
+  -- =========================================================
+
   bg = "#000000",
   fg = "#bcbcbc",
+
   cursorline = "#121212",
   selection = "#262626",
+
   linenr = "#585858",
   comment = "#585858",
+
   gutter_fg = "#585858",
   nontext = "#585858",
-  -- color approach
-  green = "#77b886",
-  yellow = "#f7cb92",
-  blue = "#7689d9",
-  magenta = "#8f509d",
-  cyan = "#8fb9f4",
-  black = "#000000",
-  bright_red = "#ff6347",
-  bright_orange = "#f6aa6b",
-  bright_green = "#a3d6a5",
-  bright_yellow = "#ffe56e",
-  bright_blue = "#9eabe6",
-  bright_magenta = "#b78fc2",
-  bright_cyan = "#a4c4ff",
-  -- gutter_fg = "#78808f",
-  -- nontext = "#d2d6dc",
-  -- -- element approach
-  -- bg = "#0f1c2e",
-  -- fg = "#e6e8ee",
-  -- cursorline = "#1c2b3a",
-  -- selection = "#2e3c55",
-  -- linenr = "#4a5a70",
-  -- comment = "#7d88a1",
+
+  -- =========================================================
   -- Syntax
-  keyword = "#f28fad",
-  Function = "#89b4fa",
-  string = "#f9e2af",
-  constant = "#cdd6f4",
-  type = "#cba6f7",
-  number = "#fab387",
-  boolean = "#f38ba8",
-  operator = "#94a3b8",
-  variable = "#e0def4",
+  -- =========================================================
+
+  keyword = "#eeeeee",
+  Function = "#eeeeee",
+  string = "#8a8a8a",
+  constant = "#d0d0d0",
+  type = "#eeeeee",
+
+  number = "#ff0000",
+  boolean = "#ff0000",
+
+  operator = "#d0d0d0",
+  variable = "#bcbcbc",
+
+  -- =========================================================
   -- UI
-  cursor = "#f5a97f",
-  visual = "#2e3c55",
-  search = "#f5c2e7",
-  statusline = "#1e293b",
-  menu_bg = "#1a2535",
-  menu_sel = "#3b4252",
-  fold = "#3e4a5a",
-  split = "#334155",
+  -- =========================================================
+
+  cursor = "#ff6200",
+
+  visual = "#262626",
+
+  search = "#303030",
+
+  statusline = "#121212",
+
+  menu_bg = "#1a1a1a",
+  menu_sel = "#303030",
+
+  fold = "#1a1a1a",
+  split = "#262626",
+
+  -- =========================================================
   -- Diagnostics
-  diag_error = "#f38ba8",
-  diag_warn = "#fab387",
-  diag_info = "#89dceb",
-  diag_hint = "#b4befe",
+  -- =========================================================
+
+  diag_error = "#ff0000",
+  diag_warn = "#ff6200",
+  diag_info = "#d0d0d0",
+  diag_hint = "#8a8a8a",
+
+  -- =========================================================
+  -- Extra semantic groups
+  -- Useful for Treesitter/LSP plugins.
+  -- =========================================================
+
+  error_bg = "#870000",
+  error_fg = "#eeeeee",
+
+  warning_bg = "#af0000",
+  warning_fg = "#eeeeee",
+
+  matchparen = "#585858",
+
+  pmenu_thumb = "#080808",
+
+  diff_add = "#ff6200",
+  diff_change = "#d70000",
+  diff_delete = "#585858",
+
+  spell_bad = "#870000",
+  spell_cap = "#af0000",
+  spell_rare = "#ff0000",
 }
